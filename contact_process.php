ef9b28fd1fee7220d572720ae64827034ac0c5d4
Nate Roper <njr61@pitt.edu>
2024-09-19 16:22:30 -0400

    Added contact functionality

---
+++ contact_process.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $fullName = $_POST['full_name'];
+    $phoneNumber = $_POST['phone_number'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $to = "njr61@pitt.edu";
+    $subject = "New Contact Form Submission";
+    $body = "Name: $fullName\nPhone: $phoneNumber\nEmail: $email\nMessage: $message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send the message.";
+    }
+}
+?>
